c3f9dfafaae84f9b8ad8974392140770857598da
ansermetpablo-ux <ansermet.pablo@gmail.com>
2026-06-03 17:51:07 +0200

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -60,7 +60,7 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(emailBody)
-    });
+    }); 
 
     const data = await response.json();
 
